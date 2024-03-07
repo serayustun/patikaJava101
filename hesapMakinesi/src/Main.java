8f139bbde62b020c77aea9c1c16630aa52468e99
serayustun <serayustn03@gmail.com>
2024-03-07 15:47:27 +0300

    comment your wip here

--- hesapMakinesi/src/Main.java
+++ hesapMakinesi/src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int num1, num2, select;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner input =new Scanner(System.in);
+
+        System.out.println("İlk sayıyı giriniz: ");
+        num1 = input.nextInt();
+        System.out.println("İkinci sayıyı giriniz: ");
+        num2 = input.nextInt();
+
+        System.out.println("1-Toplama\n2-Çıkartma\n3-Çarpma\n4-Bölme ");
+        System.out.println("Seçiminiz: ");
+        select = input.nextInt();
+
+        switch (select){
+            case 1:
+                System.out.println("Sonuç: " +(num1+num2));
+                break;
+            case 2:
+                System.out.println("Sonuç: " +(num1-num2));
+                break;
+            case 3:
+                System.out.println("Sonuç: " +(num1*num2));
+                break;
+            case 4:
+                System.out.println("Sonuç: " +((num2!=0) ? (num1/num2): "Sonsuz" ));
+                break;
+            default:
+                System.out.println("Yanlış seçim yaptınız");
+                break;
         }
+
+
     }
 }
